76ffe6fcd95bf6baff48a2a2fb0535d8fba9850b
Antonio Franceskin <antonio.franceskin@gmail.com>
2018-04-16 16:05:24 +0200

    Upload Heruko again 1

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,14 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-require 'faker'
 Zombie.destroy_all
 Weapon.destroy_all
 Armor.destroy_all
 
 50.times do
   Zombie.create!(
-    name: Faker::Superhero.prefix,
+    name: "#{Faker::Superhero.prefix} #{Faker::RickAndMorty.character}",
     hit_points: Faker::Number.between(1, 10),
     speed: Faker::Number.between(1, 10),
     brains_eaten: Faker::Number.between(1, 50),
@@ -22,7 +21,7 @@ end
 
 20.times do
   Weapon.create!(
-    name: Faker::Hacker.adjective,
+    name: "#{Faker::Hacker.adjective} #{Faker::Music.instrument}".capitalize,
     attack_points: Faker::Number.between(1, 10),
     durability: Faker::Number.between(1, 10),
     price: Faker::Number.between(1, 50)
@@ -33,23 +32,28 @@ ARMOR_SUFFIXES = %w(armor helmet shield).freeze
 
 20.times do
   Armor.create!(
-    name: Faker::Food.ingredient,
+    name: "#{Faker::Food.ingredient} #{ARMOR_SUFFIXES.sample}".capitalize,
     defense_points: Faker::Number.between(1, 10),
     durability: Faker::Number.between(1, 10),
     price: Faker::Number.between(1, 50)
   )
 end
 
+
 50.times do
+  idarmor = Armor.order("RAND()").first
+  idzombie = Zombie.order("RAND()").first
   ZombieArmor.create!(
-    zombie_id: Faker::Number.between(1, 50),
-    armor_id: Faker::Number.between(1, 20)
+    zombie_id: idzombie.id,
+    armor_id: idarmor.id
   )
 end
 
 50.times do
+  idweapon = Weapon.order("RAND()").first
+  idzombie = Zombie.order("RAND()").first
   ZombieWeapon.create!(
-    zombie_id: Faker::Number.between(1, 50),
-    weapon_id: Faker::Number.between(1, 20)
+    zombie_id: idzombie.id,
+    weapon_id: idweapon.id
   )
 end
